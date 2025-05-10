cca83276c731cd7eb2efd07a203bc09bb687b1e3
CaioRdc-Aluno <caio.costa43@aluno.ce.gov.br>
2025-05-10 04:23:13 +0000

    login

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ const email = document.getElementById("email");
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
+  e.preventDefault();
 });
